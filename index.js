2ad40fdf51144c44350ea7e9487797e81c3a5440
Faizol Aremu <waliufaizol@gmail.com>
2026-07-16 09:27:46 -0700

    add additional music entry for Mr Money and update response format

--- index.js
+++ index.js
@@ -14,11 +14,17 @@ const music = [
         artist: "Davido",
         songUrl: "https://res.cloudinary.com/wqog2nd8/video/upload/v1784120624/Davido-feat-Scotts-Maphuma-Shoday-And-TxC-Al-Xapo-Zlatan-Nakupenda-_CeeNaija.com_uox2py.mp3",
         imageUrl: "https://res.cloudinary.com/wqog2nd8/image/upload/v1784122149/images_qezvim.jpg",
+    },
+    {
+        id: 3,
+        artist: "Mr Money",
+        songUrl: "https://res.cloudinary.com/wqog2nd8/video/upload/v1784118600/Asake_-_Omo_Ope__feat._Olamide___Official_Video_128k_qpqttq.mp3",
+        imageUrl: "https://res.cloudinary.com/wqog2nd8/image/upload/v1784122025/images_bwfdus.jpg",
     }
 ]
 
 app.get('/', (req, res) => {
-    res.send('Music api')
+    res.send("Music api")
 });
 
 app.get('/musicapi', (req, res) => {
